c50e9f78389b81a50a31535366b871f3d6e9f65d
BanyelaneYT <erickda.cji@gmail.com>
2023-04-15 19:47:54 -0500

    2.0
    actualizado

--- DNI/index.php
+++ DNI/index.php
@@ -6,32 +6,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <script src="js/jquery.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="estilo.css">  
 </head>
-<body>
+<body  style="background-image: url('img/fondo1.png'); background-attachment: fixed; background-repeat: no-repeat; background-size: cover">
+<div class="text-center">
+    <div class="card-body">
+        <div class="container">
+                    <div class="container">
+                <div class="row">
+                <div class="col-lg-10 col-xl-9 opacity-50 mx-auto" style="max-width: 45rem;">
+                    <div class="card flex-row my-5 border-0 shadow rounded-3 overflow-hidden modal-content">
+                    <div class="card-img-left d-none d-md-flex">
+                        <!-- Background image for card set in CSS! -->
+                    </div>
+                    <div class="card-body p-4 p-sm-5 ">
+                        <h5 class="card-title text-center mb-5 fw-light fs-1"><a style="font-family: Times New Roman;"><big>CONSULTA DNI</big></a></h5>
+                                <div class="mb-3 ">
+                                    <input class="form-control" type="number" id="dni" Placeholder="Ingrese su número de DNI..." autocomplete="off" name="dni">
+                                </div><br>
+                                <center><a type="button" class="btn btn-primary" id="prueba"><b class="fs-4">Consultar</b></a></center><br>
+                                <div class="form-group form-control">
+                                    <div>Nombres: <label id="nombre"> </label></div>
+                                </div><br>
 
-<a href="ruc.php">Consulta RUC</a>
-<br>
-<br>
-<br>
-<br>
-
-<input type="text" id="dni" autocomplete="off" name="dni">
-
-    <button id="prueba">Consultar</button>
-
-  
-<br>
-<br>
-
-
-  <div >Nombres: <label id="nombre"> </label></div>
-
-   <div >Apellido P.: <label id="apellidop">  </label ></div>
-
-   <div> Apellido M.: <label  id="apellidom"> </label > </div>
-
-
-
+                                <div class="d-grid mb-2 form-control">
+                                    <div >Apellido P.: <label id="apellidop">  </label ></div>
+                                </div><br>
+                                
+                                <div class="d-grid mb-2 form-control">
+                                    <div> Apellido M.: <label  id="apellidom"> </label > </div>
+                                </div><br>
+                                <div></div>
+                                <div class="d-grid mb-2">
+                                    <center><a type="button" class="btn btn-primary" href="ruc.php"><b class="fs-4">Consultar RUC</b></a></center>
+                                </div>
+                    </div>
+                </div>
+             </div>
+              </div>
+        </div>
+   </div>
+</div>
 
 
 <script>
