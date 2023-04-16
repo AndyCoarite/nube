272ea33718dc429315c58b050f39dac998667b4b
BanyelaneYT <erickda.cji@gmail.com>
2023-04-15 19:55:56 -0500

    2.1
    2.1

--- DNI/index.php
+++ DNI/index.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="estilo.css">  
 </head>
-<body  style="background-image: url('img/fondo1.png'); background-attachment: fixed; background-repeat: no-repeat; background-size: cover">
+<body  style="background-image: url('fondo1.png'); background-attachment: fixed; background-repeat: no-repeat; background-size: cover">
 <div class="text-center">
     <div class="card-body">
         <div class="container">
